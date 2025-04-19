82e9ca4b6017faef327eac25c285ae5690f7b16b
ZoitovNurillo <nurillozoitov97@gmail.com>
2025-04-19 14:21:35 +0500

    Add react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
 import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import MainLayouts from './MainLayouts/MainLayouts';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Blog from './pages/Blog/Blog';
+import Newsletter from './pages/NewsLetter/Newsletter';
+
 
 function App() {
-
+  const routes = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayouts/>,
+      children: [
+        {
+          index: true,
+          element:<Home/>
+        },
+        {
+          path:"/about",
+          element:<About/>
+        },
+        {
+          path:"/blog",
+          element:<Blog/>
+        },
+        {
+          path:"/newsletter",
+          element:<Newsletter/>
+        }
+      ],
+    }
+  ])
   return (
-    <div>Hello</div>
+    <RouterProvider router={routes}/>
   )
 }
 
