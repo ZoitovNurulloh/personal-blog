db061b763cce4c8ada9013d11920fac375eb914f
ZoitovNurillo <nurillozoitov97@gmail.com>
2025-04-19 21:00:45 +0500

    Add nav component

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,23 +2,25 @@ import "./Nav.css";
 import { Link, NavLink } from "react-router-dom";
 import Button from "../Button/Button";
 
-const Nav = ({darkModeToggle,  darkMode}) => {
+const Nav = () => {
+
   return (
     <header className="header">
       <div className="header__container container">
-        <Link to="/">
+        <div className="header__wrapper">
+        <Link className="header__logo" to="/">
           <img src="/images/image-avatar.jpg"
-            className="header__logo" alt="Logo avatar" width="40" height="40" />
+            className="header__img" alt="Logo avatar" width="40" height="40" />
         </Link>
 
         <nav className="header__nav">
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/about">About</NavLink>
-          <NavLink to="/blog">Blog</NavLink>
-          <NavLink to="/newsletter">Newsletter</NavLink>
+          <NavLink className="header__link" to="/">Home</NavLink>
+          <NavLink className="header__link" to="/about">About</NavLink>
+          <NavLink className="header__link" to="/blog">Blog</NavLink>
+          <NavLink className="header__link" to="/newsletter">Newsletter</NavLink>
         </nav>
-
-        <Button darkModeToggle={darkModeToggle} darkMode={darkMode}/>
+        <Button  />
+        </div>
       </div>
     </header>
   )
