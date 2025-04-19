2e27277331d0da4c915c585022bec3b937fa83be
ZoitovNurillo <nurillozoitov97@gmail.com>
2025-04-19 21:04:14 +0500

    Update button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -20,8 +20,8 @@ const Button = () => {
   return (
         <>
         <button className="btn" onClick={() => darkModeToggle()}>
-          {darkMode && <img className="btn-img" src="/public/images/icon-sun.svg" width="14" height="14"/>}
-          {!darkMode && <img className="btn-img" src="/public/images/icon-moon.svg" width="14" height="14"/>}
+          {darkMode && <img className="btn-img" src="/images/icon-sun.svg" width="14" height="14"/>}
+          {!darkMode && <img className="btn-img" src="/images/icon-moon.svg" width="14" height="14"/>}
         </button>
         </>
   )
