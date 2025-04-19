f3a57a2f22590f610f0abebe5a41fc19272df8da
ZoitovNurillo <nurillozoitov97@gmail.com>
2025-04-19 21:00:20 +0500

    Add button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,15 +1,29 @@
 import "./Button.css";
-const Button = ({darkModeToggle, darkMode}) => {
+import { useState, useEffect } from "react";
+const Button = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  function darkModeToggle() {
+    setDarkMode(prev => !prev)
+  }
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }, [darkMode]);
+
 
 
   return (
-      <button onClick={darkModeToggle}>
-        {darkMode ? (
-          <img src="/images/icon-sun.svg" alt="Sun" />
-        ) : (
-          <img src="/images/icon-moon.svg" alt="Moon" />
-        )}
-      </button>
+        <>
+        <button className="btn" onClick={() => darkModeToggle()}>
+          {darkMode && <img className="btn-img" src="/public/images/icon-sun.svg" width="14" height="14"/>}
+          {!darkMode && <img className="btn-img" src="/public/images/icon-moon.svg" width="14" height="14"/>}
+        </button>
+        </>
   )
 }
 
