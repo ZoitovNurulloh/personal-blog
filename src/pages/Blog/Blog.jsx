9046c9c1e465f8226e51ac2279d0a9b1778fd924
ZoitovNurillo <nurillozoitov97@gmail.com>
2025-04-23 19:10:18 +0500

    Add blog page

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,7 +1,19 @@
+import Articles from "../../components/Articles/Articles";
+import './Blog.css';
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <section className="blog">
+      <div className="blog-container container-main">
+        <div className="blog-wrapper">
+          <h2 className="blog-title">
+            My Articles
+          </h2>
+          <p className="blog__info">Below are all my recent blog posts. Click on any title to read the full article.</p>
+          <Articles detailed={true}/>
+        </div>
+      </div>
+    </section>
   )
 }
 
