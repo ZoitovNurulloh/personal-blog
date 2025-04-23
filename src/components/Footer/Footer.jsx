61d4e49f962b098cc7f21b678b951b3908206894
ZoitovNurillo <nurillozoitov97@gmail.com>
2025-04-23 18:22:03 +0500

    Add footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,17 @@
+import './Footer.css';
+import '../NavFooter/NavFooter';
+import NavFooter from '../NavFooter/NavFooter';
 
 const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <footer className='footer'>
+      <div className="footer__container container-main">
+        <div className="footer__wrapper">
+          <h4 className="footer__title">Made with ❤️ and ☕️</h4>
+          <NavFooter/>
+        </div>
+      </div>
+    </footer>
   )
 }
 
